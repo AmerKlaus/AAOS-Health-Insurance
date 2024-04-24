a7c44ac655aaa378cb209f688908f970c2b9f9d0
Olivierleone <fatecasual@gmail.com>
2024-04-23 22:46:38 -0400

    Implemented QR functionality, successfully syncing with Authenticator. Also, introduced styling enhancements for the 2FA setup interface

--- app/views/User/setup2fa.php
+++ app/views/User/setup2fa.php
@@ -1,14 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>2FA Setup</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2FA</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <style>
+        /* Custom CSS for wrapping text into two lines */
+        .twofa-page p {
+            max-width: 400px; /* Adjust the maximum width as needed */
+            margin: 0 auto; /* Center the paragraph */
+        }
+    </style>
 </head>
-<body>
-    <img height="300" width="300" src="<?php echo $data['QRCode'];?>">
-    <p>Scan the above QR-code with your mobile Authenticator app, such as Google Authenticator. The authenticator app will generate codes that are valid for 30 seconds only. Enter such a code and submit it while it is still valid to apply the 2-factor authentication protection to your account.</p>
-    <form method="post" action="">
-        <label>Current code:<input type="text" name="totp" /></label>
-        <input type="submit" name="action" value="Verify code" />
-    </form>
+<body class="twofa-page d-flex align-items-center justify-content-center">
+    <div class="text-center">
+        <img height="300" width="300" src="<?php echo $data['QRCode'];?>" class="mb-4">
+        <p>Scan the above QR-code with your mobile Authenticator app, such as Google Authenticator. The authenticator app will generate codes that are valid for 30 seconds only. Enter such a code and submit it while it is still valid to apply the 2-factor authentication protection to your account.</p>
+        <form method="post" action="" class="login-form">
+            <label>Current code:<input type="text" name="totp"></label>
+            <input type="submit" name="action" value="Verify code" class="btn btn-primary">
+        </form>
+    </div>
 </body>
 </html>
