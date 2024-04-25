a150e23ee050a09a1c40c43a6c7ca48cd82670c8
AlexC <alexander.m.cecile@gmail.com>
2024-04-25 04:11:09 -0400

    Standardized naming of claim submission date, cleaned up SQL slightly

--- app/models/Claim.php
+++ app/models/Claim.php
@@ -11,22 +11,22 @@ class Claim
     public $policy_id;
     public $claim_type;
     public $claim_details;
-    public $claim_date;
+    public $submission_date;
     public $status;
     public $health_card_number;
 
     // Insert a new claim into the database
     public function insert(PDO $db_conn)
     {
-        $SQL = 'INSERT INTO Claim (policy_id, user_id, claim_type, claim_details, submission_date, status, health_insurance_number) 
-                VALUES (:policy_id, :user_id, :claim_type, :claim_details, :submission_date, :status, :health_insurance_number)';
+        // Could use SQL function CURDATE() as the submission date
+        $SQL = 'INSERT INTO Claim VALUES (DEFAULT, :policy_id, :user_id, :claim_type, :claim_details, :submission_date, :status, :health_insurance_number)';
         $STMT = $db_conn->prepare($SQL);
         $data = [
             'policy_id' => $this->policy_id,
             'user_id' => $this->user_id,
             'claim_type' => $this->claim_type,
             'claim_details' => $this->claim_details,
-            'submission_date' => $this->claim_date,
+            'submission_date' => $this->submission_date,
             'status' => $this->status,
             'health_insurance_number' => $this->health_card_number
         ];
@@ -48,23 +48,12 @@ class Claim
         $SQL = 'SELECT * FROM Claim WHERE claim_id = :claimId';
         $STMT = $db_conn->prepare($SQL);
         $STMT->execute(['claimId' => $claimId]);
-        $result = $STMT->fetch(PDO::FETCH_ASSOC);
+        $result = $STMT->fetch(PDO::FETCH_CLASS, 'app\models\Claim');
 
         if ($result) {
-            // Create a new Claim instance and populate its attributes
-            $claim = new Claim();
-            $claim->claim_id = $result['claim_id'];
-            $claim->user_id = $result['user_id'];
-            $claim->policy_id = $result['policy_id'];
-            $claim->claim_type = $result['claim_type'];
-            $claim->submission_date = $result['submission_date'];
-            $claim->status = $result['status'];
-            $claim->claim_details = $result['claim_details'];
-            $claim->health_insurance_number = $result['health_insurance_number'];
-
             return $claim;
         } else {
-            return null; // Claim not found
+            return null;
         }
     }
 
