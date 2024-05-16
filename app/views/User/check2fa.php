5ae2c8706ffbbe14685c96fc23b609b71d9ca00b
Olivierleone <fatecasual@gmail.com>
2024-05-15 23:56:19 -0400

    Added language change in forgot password,and both "Forgot Password" and "Check 2FA" functionalities were internalized.

--- app/views/User/check2fa.php
+++ app/views/User/check2fa.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php echo htmlspecialchars($_COOKIE['lang'] ?? 'en'); ?>">
 
 <head>
     <!-- Meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Verify 2FA</title>
+    <title><?php echo _("Verify 2FA"); ?></title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="../css/style.css" rel="stylesheet" type="text/css">
@@ -14,10 +14,10 @@
 
 
 <body>
-    <p>Submit the 6-digit code for this site from your Authenticator app.</p>
+    <p><?php echo __("Submit the 6-digit code for this site from your Authenticator app."); ?></p>
     <form method="post" action="">
-        <label>Current code:<input type="text" name="totp" /></label>
-        <input type="submit" name="action" value="Verify code" />
+        <label><?php echo __("Current code:"); ?><input type="text" name="totp" /></label>
+        <input type="submit" name="action" value="<?php echo __("Verify code"); ?>" />
     </form>
 </body>
 
