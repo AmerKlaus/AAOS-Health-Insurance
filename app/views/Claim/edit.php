d90770a1bb769994f9e47aed4ec147a1bcf2207e
Amer Al Jawabra <95725437+AmerKlaus@users.noreply.github.com>
2024-05-17 10:30:58 -0400

    Update edit.php

--- app/views/Claim/edit.php
+++ app/views/Claim/edit.php
@@ -12,7 +12,7 @@
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <!-- Custom CSS -->
-    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link href="/css/style.css" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
